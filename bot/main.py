dd974c9b1aab79f4b2d3527ed5975308ecf5f94d
anna_kovalenko_4 <notmelife@mail.ru>
2025-09-18 18:36:48 +0300

    change main.py

--- bot/main.py
+++ bot/main.py
@@ -90,4 +90,4 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("bot.main:app", host="0.0.0.0", port=port, log_level="info")
